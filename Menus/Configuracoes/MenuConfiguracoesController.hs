fa4ab415f410c3b2138242f47624e40265b1c52f
kpzinnm <gabriel.santos.alves@ccc.ufcg.edu.br>
2024-03-16 22:46:24 -0300

    adicionando funcionadade de salvar Admin no JSON, incompleta

--- Menus/Configuracoes/MenuConfiguracoesController.hs
+++ Menus/Configuracoes/MenuConfiguracoesController.hs
@@ -7,11 +7,46 @@
 module Menus.Configuracoes.MenuConfiguracoesController where
 import System.IO ( hFlush, stdout )
 import Utilitarios.Matriz.MatrizServices ( printMatrix )
+import Data.Char (toUpper)
+import Control.Concurrent ( threadDelay )
+import Modelos.Administrador
+import Utilitarios.MenuConfiguracoes.Administrador.ManipulaAdministrador ( adicionarAdministradorJSON )
 
 startMenuConfiguracao :: IO()
 startMenuConfiguracao = do
     printMatrix "./Interfaces/Configuracoes/menuConfiguracoes.txt"
-    putStr "Informe seu usuario: "
+    putStr "Digite uma opção: "
     hFlush stdout
     userChoice <- getLine
-    print userChoice
+    let userChoiceUpper = map toUpper userChoice
+    escolhaMenu userChoiceUpper
+
+escolhaMenu :: String -> IO()
+escolhaMenu userChoice
+    | userChoice == "L" = loginAdministrador
+    | userChoice == "A" = adicionarAdministrador
+    | otherwise = do
+        putStrLn "\nOpção inválida!"
+        threadDelay 700000
+        startMenuConfiguracao
+
+
+loginAdministrador :: IO()
+loginAdministrador = do
+    print "ai pa"
+    
+    
+
+adicionarAdministrador :: IO()
+adicionarAdministrador = do
+    printMatrix "./Interfaces/Configuracoes/menuConfiguracoesLogin.txt"
+    putStr "Digite seu user: "
+    hFlush stdout
+    user <- getLine
+    putStr "Digite sua senha: "
+    hFlush stdout
+    senha <- getLine
+    let administrador = Administrador 0 user senha
+   -- putStrLn $ show administrador
+    adicionarAdministradorJSON administrador
+    startMenuConfiguracao
--- Modelos/Administrador.hs
+++ Modelos/Administrador.hs
@@ -1,5 +1,9 @@
 module Modelos.Administrador where
 
+import GHC.Generics ( Generic )
+
 data Administrador = Administrador {
-    
-}
+    ident:: Int,
+    login:: String,
+    senha:: String
+} deriving (Show, Generic)
